c0942ac4456f230265bb335112c443fc2717115a
autoshgame <66913909+autoshgame@users.noreply.github.com>
2021-06-09 17:51:59 +0700

    Add player horizontal movement, shooting prefabs, adjust prefabs
    Create new banana prefab that can be instantiated by pressing the space
    key. This prefab can be destroyed if it touchs the screen's edge. Rotate
    all animal prefabs by 180 degrees in the Course Library folder.

---
+++ Assets/Scripts/FoodsMoving.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodsMoving : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject foods;
+    [SerializeField]
+    protected float speed = 10f;
+    protected float topBound = 23f;
+
+
+    private void clearFoods()
+    {
+        if(foods.transform.position.z > topBound)
+        {
+            Destroy(foods);
+            Debug.Log("food is destroyed");
+        }
+    }
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foods.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        clearFoods();
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject player;
+    [SerializeField]
+    protected GameObject Bananas;
+    [SerializeField]
+    protected float speed = 10f;
+    protected float horizontalInput;
+    protected float xRange = 16;
+
+    //Make player stop when reaching a certain position
+    public void stopPlayer()
+    {
+        if (player.transform.position.x > xRange)
+        {
+            player.transform.position = new Vector3(xRange, player.transform.position.y, player.transform.position.z);
+        }
+        else if (player.transform.position.x < -xRange)
+        {
+            player.transform.position = new Vector3(-xRange, player.transform.position.y, player.transform.position.z);
+        }
+    }
+
+    //Throw foods into Animals
+    public void throwFoods()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Bananas, transform.position, Bananas.transform.rotation);
+        }
+    }
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        player.transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        stopPlayer();
+        throwFoods();
+    }
+
+}
