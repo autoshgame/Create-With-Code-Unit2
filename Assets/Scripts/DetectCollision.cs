b353f1f882b7e3c7dd1c84b3540582ee1393a4cb
autoshgame <66913909+autoshgame@users.noreply.github.com>
2021-06-10 16:20:10 +0700

    Detect collision between players and animals
    Create new script file to detect collision. Add it to all animals
    prefabs and enjoy the show :v

---
+++ Assets/Scripts/DetectCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+        Destroy(other.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
