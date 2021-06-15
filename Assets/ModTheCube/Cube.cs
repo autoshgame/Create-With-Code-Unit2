9afcff06b1fc4d0778fb7ee57f6799ebbf3998d0
autoshgame <66913909+autoshgame@users.noreply.github.com>
2021-06-15 12:04:46 +0700

    Complete challange :v
    Things have been done
    * Observe the default script functionality
    * Modify the cube.cs script
    * Publish to web.gl file

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
+    [SerializeField] Material cubeMaterial;
+    [SerializeField] protected float speed = 10f;
     
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
+        transform.position = new Vector3(0, 0, 0);
+        transform.localScale = Vector3.one * 1.5f;
+
+        //Material material = Renderer.material;
         
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+       //material.color = new Color(1.0f, 1.0f, 0.3f, 0.4f);
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(20.0f * Time.deltaTime * speed, 0.0f, 0.0f);
     }
 }
